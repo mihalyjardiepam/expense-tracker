cfabb6c186850812db27bcbda3da75f9fd7c1303
Járdi Mihály <mihaly_jardi@epam.com>
2025-03-06 11:24:56 +0100

    remove magic numbers

--- src/frontend/app/lib/generate-random-color.ts
+++ src/frontend/app/lib/generate-random-color.ts
@@ -1,5 +1,8 @@
 export type HexColor = string;
 
+const RED_LUMINANCE = 0.299;
+const GREEN_LUMINANCE = 0.587;
+
 /**
  * Generates a pseudorandom color code with a capped luminance value.
  * @returns A random hex color code.
@@ -8,16 +11,14 @@ export function generateRandomColor(): HexColor {
   let lum = 50;
 
   const r = Math.floor(Math.max(20, Math.random() * 155 + 50 - lum));
-  lum += r * 0.299;
+  lum += r * RED_LUMINANCE;
   const g = Math.floor(Math.max(20, Math.random() * 155 + 50 - lum));
-  lum += g * 0.587;
+  lum += g * GREEN_LUMINANCE;
   let b = Math.floor(Math.max(20, Math.random() * 155 + 50 - lum));
 
   return `#${toHex(r)}${toHex(g)}${toHex(b)}`;
 }
 
 function toHex(number: number): string {
-  console.log(number);
-
   return number.toString(16).padStart(2, "0");
 }
